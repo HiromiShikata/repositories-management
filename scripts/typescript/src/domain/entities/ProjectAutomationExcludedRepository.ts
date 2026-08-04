02e1dbc03e4ff21ef72435f4292ec4783ac65b00
HiromiShikata <action@github.com>
2026-08-04 11:34:47 +0000

    test(umino-project): assert both jobs skip test-repository in the workflow test file

--- scripts/typescript/src/domain/entities/ProjectAutomationExcludedRepository.ts
+++
@@ -1,7 +0,0 @@
-export const PROJECT_AUTOMATION_EXCLUDED_REPOSITORIES: string[] = [
-  'HiromiShikata/test-repository',
-];
-
-export const projectAutomationRepositoryGuard = (
-  repositoryFullName: string,
-): string => `github.repository != '${repositoryFullName}'`;
